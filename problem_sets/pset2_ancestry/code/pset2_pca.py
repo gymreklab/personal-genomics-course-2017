e06dd6b0ca9d6b15d93970d5ca0032a9ac037058
mgymrek <mgymrek@mit.edu>
2016-11-09 23:29:15 +0100

    updating pca script

--- problem_sets/pset2_ancestry/code/pset2_pca.py
+++ problem_sets/pset2_ancestry/code/pset2_pca.py
@@ -5,20 +5,19 @@ CSE291 - Personal Genomics for Bioinformaticians
 Problem Set 2 - Ancestry: Principal components analysis
 
 ./pset2_pca.py \
-  --vcf ../data/ps2_reference_genotypes.vcf.gz \
+  --data-prefix ../data/ps2_pca \
   --labels ../data/ps2_reference_labels.csv \
   --num-samples 100 \
   --num-snps 1000
 """
 
 import argparse
-import cPickle
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd # TODO maybe a way to load matrix without pandas
+from sklearn.decomposition import PCA
 import sys
 import vcf
-
-MINMAF = 0.1
 
 pop_to_color = {
     "ACB": "blue",
@@ -50,41 +49,6 @@ pop_to_color = {
     "None": "gray"
 }
 
-def convert_vcf_to_matrix(vcffile, labelfile=None, \
-                              numsamples=-1, numsnps=-1):
-    """Function to convert VCF to matrix ready for PCA
-    
-    Args:
-        vcffile (str): Path to input VCF file.
-        labelfile (str): Path to population label CSV file. If
-            not provided, use lable "Unknown".
-        numsamples (int): Number of samples to use. -1 for all (default).
-        numsnps (int): Number of SNPs to use. -1 for all (default).
-
-    Returns:
-        gtmatrix (np.array): Float matrix of size numsamples x numsnps.
-                 0, 0.5, 1 for AA, AB, BB
-        samplelabels (list[str]): List of sample population labels.
-    """
-    # Get sample->pop dictionary
-    if labelfile is not None:
-        sample_to_pop = dict(map(lambda x: x.strip().split(","), open(labelfile, "r").readlines()))
-    else: sample_to_pop = {}
-    # Read SNP genotype matrix - TODO preallocate space for this to save time?
-    reader = vcf.Reader(open(vcffile, "rb"))
-    if numsamples == -1 or numsamples > len(reader.samples): numsamples = len(reader.samples)
-    snp_gts = []
-    for record in reader:
-        if len(record.alleles) > 2: continue
-        if record.aaf[0] <= MINMAF or record.aaf[0] >= (1-MINMAF): continue
-        snpdata = [sum((map(float, sample.gt_alleles)))*0.5 for sample in record]
-        snp_gts.append(snpdata)
-        if len(snp_gts) >= numsnps and numsnps != -1: break
-        if len(snp_gts)%100 == 1: sys.stderr.write("[pset2_pca.py]    Processed %s SNPs...\n"%len(snp_gts))
-    # Return results
-    return np.array(snp_gts)[:,0:numsamples], \
-        map(lambda x: sample_to_pop.get(x, "None"), reader.samples[0:numsamples])
-
 def normalize_snp_matrix(ref_matrix):
     """Function to normalize each SNP according to allele frequency
     
@@ -93,12 +57,11 @@ def normalize_snp_matrix(ref_matrix):
 
     Returns:
         ref_matrix_norm (np.array): Float matrix of normalized SNPs, size numsamples x numsnps
-        snp_p (np.array): Float array of allele frequencies. size numsnps
     """
     snp_p = np.mean(ref_matrix, axis=1, dtype=np.float64).reshape(ref_matrix.shape[0], 1)
     snp_se = np.sqrt(snp_p*(1-snp_p)).reshape(ref_matrix.shape[0], 1)
     ref_matrix_norm = (ref_matrix-snp_p)/snp_se
-    return ref_matrix_norm, snp_p
+    return ref_matrix_norm
 
 def perform_pca(ref_matrix_norm):
     """Perform PCA on normalized matrix
@@ -108,16 +71,25 @@ def perform_pca(ref_matrix_norm):
 
     Returns:
         ref_matrix_projection (np.array): Float matrix of data projected onto PCs. size numsamples x numsnps
-        eigvecs (np.array): Matrix of eigenvectors. size numsamples x numsamples
     """
-    # TODO are these sorted?
+    """
     # Get GRM (covariance matrix)
     grm = ref_matrix_norm.transpose().dot(ref_matrix_norm)/ref_matrix_norm.shape[0]
-    # Get Eigendecomposition
+    # Get Eigendecomposition - column i is ith eigenvector
     eigvals, eigvecs = np.linalg.eig(grm)
     # Project data onto PCs
+    print eigvecs.shape
+    print ref_matrix_norm.shape
     ref_matrix_projection = eigvecs.transpose().dot(ref_matrix_norm.transpose())
-    return ref_matrix_projection, eigvecs
+    return ref_matrix_projection
+    """
+    pca = PCA(n_components=2)
+    pca.fit(ref_matrix_norm.transpose())
+    return pca.transform(ref_matrix_norm.transpose())
+    
+# eig: column i is ith eigenvector numsamples x numsamples
+# ref_matrix_norm: numsamples x numsnps
+
 
 def plot_ref_panel(pc1, pc2, colors, outprefix):
     """Function to plot top PCs of reference panel
@@ -145,57 +117,44 @@ def plot_ref_panel(pc1, pc2, colors, outprefix):
     ax.get_yaxis().tick_left();
     fig.savefig(outprefix + ".pdf")
 
-def read_pca_data(outprefix, numsamples=-1):
+def read_pca_data(dataprefix, labelfile, numsamples=-1):
     """Function to read matrix that was pre-calcluated
     Args:
-        outprefix (str): Prefix of files to load
+        dataprefix (str): Prefix of files to load
+        labelfile (str): Path to population label CSV file. If
+            not provided, use lable "Unknown".
         numsamples (int): Number of samples to use. -1 for all (default).
 
     Returns:
         ref_matrix (np.array): Float matrix of size numsamples x numsnps
         ref_labels (list[str]): List of sample population labels.
     """
-    ref_matrix = cPickle.load(open(outprefix+".ref_matrix.pkl", "r"))
-    ref_labels = cPickle.load(open(outprefix+".ref_labels.pkl", "r"))
-    if numsamples != -1:
-        ref_matrix = ref_matrix[:,0:numsamples]
-        ref_labels = ref_labels[0:numsamples]
+    ref_matrix = np.array(pd.read_csv(dataprefix+".genotypes.tab", sep="\t", header=False))
+    sample_labels = map(lambda x: x.strip(), open(dataprefix + ".samples.txt", "r").readlines())
+    # Get sample->pop dictionary
+    if labelfile is not None:
+        sample_to_pop = dict(map(lambda x: x.strip().split(","), open(labelfile, "r").readlines()))
+    else: sample_to_pop = {}
+    ref_labels = [sample_to_pop.get(s, "None") for s in sample_labels]
     return ref_matrix, ref_labels
-    
-def write_pca_data(ref_matrix, ref_labels, outprefix):
-    """Function to write matrix so we don't have to recalculate each time.
-
-    Args:
-        ref_matrix (np.array): Float matrix of size numsamples x numsnps
-        ref_labels (list[str]): List of sample population labels. 
-        outprefix (str): Output prefix for files
-    """
-    cPickle.dump(ref_matrix, open(outprefix+".ref_matrix.pkl", "w"))
-    cPickle.dump(ref_labels, open(outprefix+".ref_labels.pkl", "w"))
     
 def main():
     parser = argparse.ArgumentParser(__doc__)
-    parser.add_argument("--vcf", help="Reference panel VCF", type=str, required=True)
-    parser.add_argument("--labels", help="CSV file with sample, pop label for ref panel", type=str, required=True)
     parser.add_argument("--num-samples", help="Number of samples to use from ref panel. -1 for all.", type=int, default=-1)
     parser.add_argument("--num-snps", help="Number of SNPs to include in analysis. -1 for all.", type=int, default=-1)
     parser.add_argument("--out", help="Prefix for output files.", type=str, default="ps2_pca")
-    parser.add_argument("--load-from-pkl", help="Load matrix from pickle (for debugging)", action="store_true")
+    parser.add_argument("--data-prefix", help="Data prefix for precomputed genotype matrix", type=str, required=True)
+    parser.add_argument("--labels", help="CSV file with sample, pop label for ref panel", type=str)
     args = parser.parse_args()
 
-    sys.stderr.write("[pset2_pca.py] Convert reference panel VCF to matrix...\n")
-    if args.load_from_pkl:
-        ref_matrix, ref_labels = read_pca_data(args.out, numsamples=args.num_samples)
-    else:
-        ref_matrix, ref_labels = convert_vcf_to_matrix(args.vcf, args.labels, \
-                                                       numsamples=args.num_samples, numsnps=args.num_snps)
-        write_pca_data(ref_matrix, ref_labels, args.out)
+    sys.stderr.write("[pset2_pca.py] Read data for PCA...\n")
+    ref_matrix, ref_labels = read_pca_data(args.data_prefix, args.labels, numsamples=args.num_samples)
     
     sys.stderr.write("[pset2_pca.py] Normalize SNP genotypes...\n")
-    ref_matrix_norm, allele_freqs = normalize_snp_matrix(ref_matrix)
-    
+    ref_matrix_norm = normalize_snp_matrix(ref_matrix)
+
     sys.stderr.write("[pset2_pca.py] Perform PCA...\n")
-    ref_matrix_projection, eigvecs = perform_pca(ref_matrix_norm)
+    ref_matrix_projection = perform_pca(ref_matrix_norm)
 
     sys.stderr.write("[pset2_pca.py] Plot reference panel...\n")
     plot_ref_panel(ref_matrix_projection[:,0], ref_matrix_projection[:, 1], \
