e3b86351bf1e2531503b8d50e4145ff8c901b575
Melissa Ann Gymrek <mgymrek@comet-ln3.sdsc.edu>
2016-11-28 21:57:29 -0800

    adding util to load modules

---
+++ utils/load_modules.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#################################################
+# CSE291: Bioinformatics for personal genomics
+
+# This script loads modules used in the course
+#################################################
+
+module load bcftools/1.3
+module load bedtools/2.25.0
+
+
+
