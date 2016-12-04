3832973e0e9315e9cc0725c5e633e47d93d3286b
Melissa Ann Gymrek <mgymrek@comet-ln3.sdsc.edu>
2016-12-03 21:26:02 -0800

    adding ibd analysis

--- problem_sets/pset2_ancestry/code/preprocess_ibd.sh
+++ problem_sets/pset2_ancestry/code/preprocess_ibd.sh
@@ -5,25 +5,32 @@
 #SBATCH -t 1000
 #SBATCH --get-user-env
 
-INVCF=/oasis/projects/nsf/csd524/mgymrek/data/ps2/ALL.chr16.integrated_phase1_v3.20101123.snps_indels_svs.genotypes.vcf.gz
-SUBVCF=/oasis/projects/nsf/csd524/mgymrek/data/ps2/ps2_ibd.vcf.gz
-PREFIX=/oasis/projects/nsf/csd524/mgymrek/data/ps2/ps2_ibd
+DATADIR=/oasis/projects/nsf/csd524/mgymrek/data/ps2/
+PREFIX=${DATADIR}/ps2_ibd
 
-#	vcf-subset -c NA19713,NA19985,NA19625,NA20414,NA19660,NA19685 \
-
-# Get subset of samples
+# Get VCF
 for chrom in $(seq 1 22)
 do
-    zcat /oasis/projects/nsf/csd524/mgymrek/data/ps2/ALL.chr${chrom}.integrated_phase1_v3.20101123.snps_indels_svs.genotypes.vcf.gz | grep -v "VT=SV" | \
+    zcat ${DATADIR}/ALL.chr${chrom}.integrated_phase1_v3.20101123.snps_indels_svs.genotypes.vcf.gz | \
+	grep -v "VT=SV" | \
 	intersectBed -header -a stdin -b 23andme_snps_hg19.bed.gz \
-	 > /oasis/projects/nsf/csd524/mgymrek/data/ps2/ps2_ibd_${chrom}.vcf
-    bgzip -f /oasis/projects/nsf/csd524/mgymrek/data/ps2/ps2_ibd_${chrom}.vcf
-    tabix -f -p vcf /oasis/projects/nsf/csd524/mgymrek/data/ps2/ps2_ibd_${chrom}.vcf.gz
+	 > ${PREFIX}_${chrom}.vcf
+    bgzip -f ${PREFIX}_${chrom}.vcf
+    tabix -f -p vcf ${PREFIX}_${chrom}.vcf.gz
 done
 
 # concat chrom VCFs
-vcf-concat $(ls /oasis/projects/nsf/csd524/mgymrek/data/ps2/ps2_ibd_*.vcf.gz) | bgzip -c > ${SUBVCF}
-tabix -f -p vcf ${SUBVCF}
+vcf-concat $(ls ${PREFIX}_*.vcf.gz) | bgzip -c > ${PRERIX}.vcf.gz
+tabix -f -p vcf ${PREFIX}.vcf.gz
 
-# Convert to plink format
-vcftools --gzvcf ${SUBVCF} --plink-tped --out ${PREFIX}
+# Subset
+zcat ${PREFIX}.vcf.gz | vcf-subset -f -c LWK.list | uniq | bgzip -c > ${PREFIX}.lwk.vcf.gz
+tabix -f -p vcf ${PREFIX}.lwk.vcf.gz
+plink --vcf ${PREFIX}.lwk.vcf.gz \
+    --cm-map ${DATADIR}/imputation/1000GP_Phase3/genetic_map_chr@_combined_b37.txt \
+    --make-bed \
+    --out ${PREFIX}.lwk
+
+# Remove chrom files
+rm ${PREFIX}_*.vcf.gz
+rm ${PREFIX}_*.vcf.gz.tbi
--- problem_sets/pset2_ancestry/code/run_ps2_ibd.sh
+++ problem_sets/pset2_ancestry/code/run_ps2_ibd.sh
@@ -2,18 +2,12 @@
 
 #SBATCH -A csd524
 #SBATCH -p compute
-#SBATCH -t 10
+#SBATCH -t 1000
 #SBATCH --get-user-env
 
-#java -jar -Xmx512m /home/mgymrek/sources/beagle.r1399.jar \
-#    gt=/oasis/projects/nsf/csd524/mgymrek/data/ps2/ps2_ibd.vcf.gz \
-#    out=/oasis/projects/nsf/csd524/$USER/ps2/results/ps2_ibd \
-#    ibd=true impute=false
+DATADIR=/oasis/projects/nsf/csd524/mgymrek/data/ps2/
+PREFIX=${DATADIR}/ps2_ibd
+OUTDIR=/oasis/projects/nsf/csd524/mgymrek/ps2/results/
 
-#cat /oasis/projects/nsf/csd524/$USER/ps2/results/ps2_ibd.ibd | \
-#    java -jar -Xmx512m /home/mgymrek/sources/ibdmerge.jar > \
-#    /oasis/projects/nsf/csd524/$USER/ps2/results/ps2_ibd.ibd.merged
+plink --bfile ${PREFIX}.lwk --genome --out ${OUTDIR}/lwk.ibd
 
-plink --tfile /oasis/projects/nsf/csd524/mgymrek/data/ps2/ps2_ibd \
-    --genome \
-    --out /oasis/projects/nsf/csd524/$USER/ps2/results/ps2_ibd_plink
