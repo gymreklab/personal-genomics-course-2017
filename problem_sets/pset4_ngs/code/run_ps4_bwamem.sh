6f09642f4ace45094af004721a63e345ca87e249
gymreklab <melissa.gymrek@gmail.com>
2017-02-06 16:42:09 -0800

    adding genome file

--- problem_sets/pset4_ngs/code/run_ps4_bwamem.sh
+++ problem_sets/pset4_ngs/code/run_ps4_bwamem.sh
@@ -5,15 +5,18 @@
 #SBATCH -t 1000
 #SBATCH --get-user-env
 
-DATADIR=/oasis/projects/nsf/csd524/mgymrek/data/ps4/
-INDEX=${DATADIR}/hs37d5.fa
+#DATADIR=/oasis/projects/nsf/csd524/mgymrek/data/ps4/
+#INDEX=${DATADIR}/hs37d5.fa
+
+DATADIR=/storage/mgymrek/cse291/ps4data/
+INDEX=/storage/resources/dbase/human/hs37d5/hs37d5.fa
 FQ1=${DATADIR}/SRR622457_1.fastq.gz
 FQ2=${DATADIR}/SRR622457_2.fastq.gz
 
-#bwa mem -R '@RG\tID:NA12878\tSM:NA12878' ${INDEX} ${FQ1} ${FQ2} > ${DATADIR}/NA12878.sam
+bwa mem -t 5 -R '@RG\tID:NA12878\tSM:NA12878' ${INDEX} ${FQ1} ${FQ2} > ${DATADIR}/NA12878.sam
 
 # Convert to BAM, note sam is truncated but we'll just use a subset?
-cat ${DATADIR}/NA12878.sam | grep -v "SRR622457\.68907082" | \
-    samtools view -bS - > ${DATADIR}/NA12878.bam
+#cat ${DATADIR}/NA12878.sam | grep -v "SRR622457\.68907082" | \
+#    samtools view -bS - > ${DATADIR}/NA12878.bam
 
 # Sort
