48d60d33a083f39d84f5c1a1a54215a064fddfba
Melissa Ann Gymrek <mgymrek@comet-ln3.sdsc.edu>
2017-02-28 14:55:39 -0800

    adding preprocess exome file

--- problem_sets/pset4_ngs/code/run_ps4_comparesnps.sh
+++ problem_sets/pset4_ngs/code/run_ps4_comparesnps.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
+#SBATCH -A csd524
+#SBATCH -p compute
+#SBATCH -t 1000
+#SBATCH --get-user-env
+
 NIST=/oasis/projects/nsf/csd524/mgymrek/data/ps4/NIST_NA12878_hg38_chr22.tab
 SNPCALLS=/oasis/projects/nsf/csd524/$USER/ps4/results/NA12878_chr22_snps.tab
 
-# Only look at non homref - otherwise this file is huge
-cat ${SNPCALLS} | awk '($4!= ".")' > ${SNPCALLS}.onlysnps
+# For testing at log in shell - Only look at non homref - otherwise this file is huge
+#cat ${SNPCALLS} | awk '($4!= ".")' > ${SNPCALLS}.onlysnps
+# SNPCALLS=${SNPCALLS}.onlysnps
 
 # Load to compare script
 ./ps4_comparesnps.py \
     --nist ${NIST} \
-    --snpcalls ${SNPCALLS}.onlysnps
+    --snpcalls ${SNPCALLS}
 
 
